18cdce3d38778e7feefa079c2971d9ba1f2ab582
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-02-19 18:55:20 +0100

    GameInitScene start

--- VerticalArchipel/Assets/Scripts/WelcomeScene/MenuScript.cs
+++ VerticalArchipel/Assets/Scripts/WelcomeScene/MenuScript.cs
@@ -9,17 +9,31 @@ public class MenuScript : MonoBehaviour
     void OnGUI()
     {
         const int buttonWidth = 120;
-        const int buttonHeight = 80;
+        const int buttonHeight = 50;
 
         if (
           GUI.Button(
             new Rect(
               Screen.width / 2 - (buttonWidth / 2),
-              (3 * Screen.height / 4) - (buttonHeight / 2),
+              (5 * Screen.height / 10) - (buttonHeight / 2),
               buttonWidth,
               buttonHeight
             ),
             "Demarrer !"
+          )
+        )
+        {
+            SceneManager.LoadScene("GameInitScene");
+        }
+        if (
+          GUI.Button(
+            new Rect(
+              Screen.width / 2 - (buttonWidth / 2),
+              (7 * Screen.height / 10) - (buttonHeight / 2),
+              buttonWidth,
+              buttonHeight
+            ),
+            "Crédits"
           )
         )
         {
