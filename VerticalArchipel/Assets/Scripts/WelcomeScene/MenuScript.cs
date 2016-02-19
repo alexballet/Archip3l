f97e5fe3e3f4a674b2de6e2a9c95a99bd7e6e077
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-02-19 15:38:47 +0100

    Welcome screen

---
+++ VerticalArchipel/Assets/Scripts/WelcomeScene/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Script of menu
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+    void OnGUI()
+    {
+        const int buttonWidth = 120;
+        const int buttonHeight = 80;
+
+        if (
+          GUI.Button(
+            new Rect(
+              Screen.width / 2 - (buttonWidth / 2),
+              (3 * Screen.height / 4) - (buttonHeight / 2),
+              buttonWidth,
+              buttonHeight
+            ),
+            "Demarrer !"
+          )
+        )
+        {
+            SceneManager.LoadScene("GameInitScene");
+        }
+    }
+}
