e8933c5eb0443ed919f482f3409388b2997ae270
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-02-20 12:09:29 +0100

    Beginning of code integration

---
+++ VerticalArchipel/Assets/Scripts/PlayingScene/Trophy/Trophy.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System;
+
+public class Trophy : MonoBehaviour
+{
+    public int Id;
+    public string Name;
+    public string Description;
+    public bool Status;
+    //public int PosX { get; private set; }
+    //public int PosY { get; private set; }
+    //public string EmptyMedalPath { get; private set; }
+    //public string ObtainedMedalPath { get; private set; }
+    //private Image EmptyMedal;
+    //private Image WonMedal;
+    //public Image Image { get; private set; }
+    //public Bonus Bonus { get; private set; }
+
+    public Trophy(int id, string name, string description) //, int posX, int posY, Bonus bonus)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Description = description;
+        //this.PosX = posX;
+        //this.PosY = posY;
+        //this.EmptyMedalPath = "C:/tempConcours/trophy-emptyMedal.png";
+        //this.ObtainedMedalPath = "C:/tempConcours/trophy-wonMedal-" + this.Id + ".png";
+        this.Status = false;
+        //this.Bonus = bonus;
+        //this.EmptyMedal = new Image { Name = "trophy" + this.Id, Source = new BitmapImage(new Uri(this.EmptyMedalPath, UriKind.RelativeOrAbsolute)), };
+        //this.WonMedal = new Image { Name = "trophy" + this.Id, Source = new BitmapImage(new Uri(this.ObtainedMedalPath, UriKind.RelativeOrAbsolute)), };
+        //this.Image = EmptyMedal;
+    }
+    public bool changeToObtained()
+    {
+        if (this.Status)
+        {
+            return false;
+        }
+        else
+        {
+            //if (!this.Bonus.applyBonus())
+            //{
+            //    return false;
+            //}
+            this.Status = true;
+            //this.Image = this.WonMedal;
+            return true;
+        }
+    }
+    public bool changeToEmpty()
+    {
+        if (!this.Status)
+        {
+            return false;
+        }
+        else
+        {
+            this.Status = false;
+            //this.Image = this.EmptyMedal;
+            return true;
+        }
+    }
+}
---
+++ VerticalArchipel/Assets/Scripts/PlayingScene/Trophy/TrophyManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrophyManager : MonoBehaviour
+{
+    public List<Trophy> Trophies { get; private set; }
+    public GameObject TrophyGameObject;
+    public event EventHandler<TrophyObtainedEventArgs> TrophyObtained;
+
+    void start()
+    {
+        var trophy1 = Instantiate(this.TrophyGameObject) as GameObject;
+        Trophy t = trophy1.gameObject.GetComponent<Trophy>();
+        if (t != null)
+        {
+            this.Trophies.Add(t);
+            t.Id = 1;
+            t.Name = "Trophée ressources";
+            t.Description = "Gain de ressources";
+        }
+        var trophy2 = Instantiate(this.TrophyGameObject) as GameObject;
+        t = trophy2.gameObject.GetComponent<Trophy>();
+        if (t != null)
+        {
+            this.Trophies.Add(t);
+            t.Id = 2;
+            t.Name = "Trophée innovation";
+            t.Description = "Gain de productivité";
+        }
+        var trophy3 = Instantiate(this.TrophyGameObject) as GameObject;
+        t = trophy3.gameObject.GetComponent<Trophy>();
+        if (t != null)
+        {
+            this.Trophies.Add(t);
+            t.Id = 3;
+            t.Name = "Trophée rapidité";
+            t.Description = "Gain de productivité";
+        }
+        var trophy4 = Instantiate(this.TrophyGameObject) as GameObject;
+        t = trophy4.gameObject.GetComponent<Trophy>();
+        if (t != null)
+        {
+            this.Trophies.Add(t);
+            t.Id = 5;
+            t.Name = "Trophée stratégie";
+            t.Description = "Gain de ..";
+        }
+        var trophy5 = Instantiate(this.TrophyGameObject) as GameObject;
+        t = trophy5.gameObject.GetComponent<Trophy>();
+        if (t != null)
+        {
+            this.Trophies.Add(t);
+            t.Id = 5;
+            t.Name = "Trophée des légendes";
+            t.Description = "Gain de ...";
+        }
+
+        foreach(Trophy tr in this.Trophies)
+        {
+            Debug.Log(tr.Name);
+            Console.WriteLine(tr.Name);
+        }
+    }
+    //public TrophyManager()
+    //{
+    //    this.Trophies = new List<Trophy>();
+
+    //    Trophy trophy1 = new Trophy(1, "Trophée ressources", "Gain de ressources"); //, 50, 50, null);
+    //    Trophy trophy2 = new Trophy(2, "Trophée innovation", "Multiplication de la productivité par 1.5"); //, 160, 50, null);
+    //    Trophy trophy3 = new Trophy(3, "Trophée rapidité", "Multiplication de la productivité par 1.5"); //, 270, 50, null);
+    //    Trophy trophy4 = new Trophy(4, "Trophée stratégie", "Une description"); //, 380, 50, null);
+    //    Trophy trophy5 = new Trophy(5, "Trophée légende", "Une description"); //, 490, 50, null);
+
+    //    this.Trophies.Add(trophy1);
+    //    this.Trophies.Add(trophy2);
+    //    this.Trophies.Add(trophy3);
+    //    this.Trophies.Add(trophy4);
+    //    this.Trophies.Add(trophy5);
+
+    //    changeTrophyToObtained(trophy2);
+    //    changeTrophyToObtained(trophy3);
+    //    changeTrophyToObtained(trophy5);
+    //}
+    public List<Trophy> getTrophies(bool status)
+    {
+        List<Trophy> value = new List<Trophy>();
+        if (this.Trophies.Count < 1)
+        {
+            return value;
+        }
+        for (int i = 0; i < this.Trophies.Count - 1; i++)
+        {
+            if (this.Trophies[i].Status == status)
+            {
+                value.Add(this.Trophies[i]);
+            }
+        }
+        return value;
+    }
+    public void changeTrophyToObtained(Trophy trophy)
+    {
+        trophy.changeToObtained();
+        if (this.TrophyObtained != null)
+        {
+            this.TrophyObtained(this, new TrophyObtainedEventArgs { Trophy = trophy });
+        }
+    }
+}
+public class TrophyObtainedEventArgs : EventArgs
+{
+    public Trophy Trophy;
+}
