20de6997ef89480dd9a0c7ac154da4ca2c554ca2
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-02-22 21:56:57 +0100

    Building constuction animation

---
+++ Archip3l/Assets/Script/Animations/Anim_BuildingConstruction.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anim_BuildingConstruction : MonoBehaviour
+{
+    private Quaternion startQuaternion;
+    private float interval;
+
+    void Start()
+    {
+        this.startQuaternion = transform.rotation;
+        this.interval = 0.5f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("left"))
+        {
+            StartCoroutine("Rotate");
+        }
+    }
+
+    IEnumerator Rotate()
+    {
+        for (;;)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                RotateLeft();
+                yield return new WaitForSeconds(this.interval);
+            }
+            RotateInit();
+            yield return new WaitForSeconds(this.interval);
+        }
+
+    }
+    void RotateLeft()
+    {
+        transform.Rotate(Vector3.forward * 20);
+    }
+    void RotateInit()
+    {
+        transform.rotation = this.startQuaternion;
+    }
+}
