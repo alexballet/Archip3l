24e691fb6e8746cb98fa2c848ac7e7a8bc2409a6
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-02-27 20:52:18 +0100

    building upgrade animation

---
+++ Archip3l/Assets/Script/Buildings/Anim_BuildingUpgrade.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anim_BuildingUpgrade : MonoBehaviour
+{
+    private float interval;
+
+    void Start()
+    {
+        this.interval = 0.01f;
+        StartCoroutine("Move");
+    }
+
+    IEnumerator Move()
+    {
+        for (;;)
+        {
+
+            MoveUp();
+            yield return new WaitForSeconds(this.interval);
+            MoveLeft();
+            yield return new WaitForSeconds(this.interval);
+            MoveDown();
+            yield return new WaitForSeconds(this.interval);
+            MoveRight();
+            yield return new WaitForSeconds(this.interval);
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    MoveUp();
+            //    yield return new WaitForSeconds(this.interval);
+            //}
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    MoveDown();
+            //    yield return new WaitForSeconds(this.interval);
+            //}
+            //yield return new WaitForSeconds(this.interval);
+        }
+
+    }
+    void MoveUp()
+    {
+        Vector3 pos = transform.position;
+        pos.y += 1;
+        transform.GetChild(0).transform.position = pos;
+    }
+    void MoveDown()
+    {
+        Vector3 pos = transform.position;
+        pos.y -= 1;
+        transform.GetChild(0).transform.position = pos;
+    }
+    void MoveLeft()
+    {
+        Vector3 pos = transform.position;
+        pos.x -= 1;
+        transform.GetChild(0).transform.position = pos;
+    }
+    void MoveRight()
+    {
+        Vector3 pos = transform.position;
+        pos.x += 1;
+        transform.GetChild(0).transform.position = pos;
+    }
+}
